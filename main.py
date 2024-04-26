78590b574bb1975c170104c945e5ff9c45b10d1f
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2024-04-25 18:19:40 -0700

    🚧 Setup repo

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+import json
 from fastapi import FastAPI
 
-app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+f = open("./data/fixtures.json")
+fixtures = json.load(f)
+app = FastAPI(
+    title="API of NIES",
+    description="API made by NIES",
+    summary="API about data of using by NIES",
+    version="0.1.0",
+    contact={
+        "name": "NIES",
+        "url": "https://github.com/niesfutbol",
+        "email": "nepo@nies.futbol",
+    },
+    license_info={
+        "name": "AGPL-3.0 license",
+        "url": "https://github.com/niesfutbol/fut_sal_api/blob/develop/LICENSE",
+    },
+)
+
+
+@app.get("/v1/fixtures/")
+def get_fixture():
+    return fixtures
